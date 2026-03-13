82940a82ad1feb6f1c768a93c4813dc0bedf5312
raj2911-tech <raj317073b@gmail.com>
2026-03-13 11:22:18 +0530

    Add JWT auth middleware and protect auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import {register, login, logout} from '../controllers/authController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
+router.post('/register',authMiddleware ,register);
+router.post('/login',authMiddleware, login);
+router.post('/logout', authMiddleware, logout);
 
 
 export default router;
